d496a098f89ea2051aef1a687f3f102cbf0e3267
Emmanuel Sambo <emmanuel@themomproject.com>
2020-10-09 14:19:34 -0500

    Refactor: inline method: get_account_info

--- app/services/jwt_auth/jwt_creator.rb
+++ app/services/jwt_auth/jwt_creator.rb
@@ -60,7 +60,12 @@ module JwtAuth
     private
 
     def update_account_info(token)
-      account = get_account_info(token)
+      user_info_response = api_client.get_user_info(token.access_token)
+      accounts = user_info_response.accounts
+      session[:ds_user_name] = user_info_response.name
+      target_account_id = Rails.configuration.target_account_id
+      account = get_account(accounts, target_account_id)
+
       api_client.config.host = account.base_uri
       session[:ds_access_token] = token.access_token
       session[:ds_account_id] = account.account_id
@@ -68,14 +73,6 @@ module JwtAuth
       session[:ds_account_name] = account.account_name
       session[:ds_expires_at] = token.expires_in.to_i.seconds.from_now.to_i
       Rails.logger.info "==> JWT: Received token for impersonated user which will expire in: #{token.expires_in.to_i.seconds / 1.hour} hour at: #{Time.at(token.expires_in.to_i.seconds.from_now)}"
-    end
-
-    def get_account_info(token)
-      user_info_response = api_client.get_user_info(token.access_token)
-      accounts = user_info_response.accounts
-      session[:ds_user_name] = user_info_response.name
-      target_account_id = Rails.configuration.target_account_id
-      account = get_account(accounts, target_account_id)
     end
 
     def get_account(accounts, target_account_id)
