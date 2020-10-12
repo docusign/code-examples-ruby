98f9ed69d7a9dbbba69d3bf8dee9970293d8bccd
Emmanuel Sambo <emmanuel@themomproject.com>
2020-10-12 18:20:47 -0500

    Refactor: reduce indentation

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,13 +4,12 @@ require 'docusign'
 
 config = Rails.application.config
 config.middleware.use OmniAuth::Builder do
-  provider :docusign, config.integration_key, config.integration_secret,
-           setup: lambda { |env|
-             strategy = env['omniauth.strategy']
-             strategy.options[:client_options].site = config.app_url
-             strategy.options[:prompt] = 'login'
-             strategy.options[:oauth_base_uri] = config.authorization_server
-             strategy.options[:target_account_id] = config.target_account_id
-             strategy.options[:allow_silent_authentication] = config.allow_silent_authentication
-           }
+  provider :docusign, config.integration_key, config.integration_secret, setup: lambda { |env|
+    strategy = env['omniauth.strategy']
+    strategy.options[:client_options].site = config.app_url
+    strategy.options[:prompt] = 'login'
+    strategy.options[:oauth_base_uri] = config.authorization_server
+    strategy.options[:target_account_id] = config.target_account_id
+    strategy.options[:allow_silent_authentication] = config.allow_silent_authentication
+  }
 end
