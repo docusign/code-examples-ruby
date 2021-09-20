a7b241630e7539a844238692efd968ba09fcb557
karissarjacobsen <73908223+karissarjacobsen@users.noreply.github.com>
2021-09-20 09:17:28 -0700

    Devdocs 5628 (#41)
    * phone auth

--- app/controllers/e_sign/eg020_phone_authentication_controller.rb
+++ app/controllers/e_sign/eg020_phone_authentication_controller.rb
@@ -1,20 +1,23 @@
 # frozen_string_literal: true
 
-class ESign::Eg021PhoneAuthenticationController < EgController
+class ESign::Eg020PhoneAuthenticationController < EgController
   def create
     minimum_buffer_min = 3
     if check_token(minimum_buffer_min)
       begin
-        results = ESign::Eg021PhoneAuthenticationService.new(request, session).call
+        results = ESign::Eg020PhoneAuthenticationService.new(request, session).call
         session[:envelope_id] = results.envelope_id
-        @title = 'Envelope sent'
-        @h1 = 'Envelope sent'
+        @title = 'Require Phone Authentication for a Recipient'
+        @h1 = 'Require Phone Authentication for a Recipient'
         @message = "The envelope has been created and sent!<br/>Envelope ID #{results.envelope_id}."
         render 'ds_common/example_done'
       rescue DocuSign_eSign::ApiError => e
         error = JSON.parse e.response_body
         @error_code = error['errorCode']
         @error_message = error['message']
+        if error['errorCode']["IDENTITY_WORKFLOW_INVALID_ID"]
+          @error_information = "Please contact <a target='_blank' rel='noopener noreferrer' href='https://support.docusign.com/'>Support</a> to enable recipient phone authentication in your account."
+        end
         render 'ds_common/error'
       end
     else
--- app/services/e_sign/eg020_phone_authentication_service.rb
+++ app/services/e_sign/eg020_phone_authentication_service.rb
@@ -1,23 +1,26 @@
 # frozen_string_literal: true
 
-class ESign::Eg021PhoneAuthenticationService
+class ESign::Eg020PhoneAuthenticationService
   include ApiCreator
   attr_reader :args, :envelope_args, :request, :session
 
   def initialize(request, session)
-    @envelope_args = {
-      signer_email: request.params['signerEmail'].gsub(/([^\w \-\@\.\,])+/, ''),
-      signer_name: request.params['signerName'].gsub(/([^\w \-\@\.\,])+/, ''),
-      status: 'sent'
-    }
+    @request = request
+    @session = session
     @args = {
       account_id: session['ds_account_id'],
       base_path: session['ds_base_path'],
       access_token: session['ds_access_token'],
       envelope_args: @envelope_args
     }
-    @request = request
-    @session = session
+    @envelope_args = {
+      signer_email: request.params['signer_email'].gsub(/([^\w \-\@\.\,])+/, ''),
+      signer_name: request.params['signer_name'].gsub(/([^\w \-\@\.\,])+/, ''),
+      country_code: request.params['country_code'].gsub(/([^\w \-\@\.\,])+/, ''),
+      phone_number: request.params['phone_number'].gsub(/([^\w \-\@\.\,])+/, ''),
+      workflow_id: get_workflow(@args),
+      status: 'sent'
+    }
   end
 
   def call
@@ -33,7 +36,7 @@ class ESign::Eg021PhoneAuthenticationService
     document1 = DocuSign_eSign::Document.new(
       # Create the DocuSign Document object
       documentBase64: Base64.encode64(File.binread(File.join('data', pdf_filename))),
-      name: 'NDA', # Can be different from actual file name
+      name: 'Lorem', # Can be different from actual file name
       fileExtension: 'pdf', # Many different document types are accepted
       documentId: '1' # A label used to reference the doc
     )
@@ -44,10 +47,31 @@ class ESign::Eg021PhoneAuthenticationService
     signer1 = DocuSign_eSign::Signer.new
     signer1.email = envelope_args[:signer_email]
     signer1.name = envelope_args[:signer_name]
+    signer1.role_name = ''
+    signer1.note = ''
+    signer1.status = 'created'
+    signer1.delivery_method = 'email'
     signer1.recipient_id = '1'
     signer1.routing_order = '1'
 
+    phone_number = DocuSign_eSign::RecipientIdentityPhoneNumber.new
+    phone_number.country_code = envelope_args[:country_code]
+    phone_number.number = envelope_args[:phone_number]
+
+    input_option = DocuSign_eSign::RecipientIdentityInputOption.new
+    input_option.name = 'phone_number_list'
+    input_option.value_type = 'PhoneNumberList'
+    input_option.phone_number_list = [phone_number]
+
+    identity_verification = DocuSign_eSign::RecipientIdentityVerification.new
+
+    identity_verification.workflow_id = session[:workflow_id]
+    identity_verification.input_options = [input_option]
+
+    signer1.identity_verification = identity_verification
+
     sign_here1 = DocuSign_eSign::SignHere.new
+    sign_here1.name = 'SignHereTab'
     sign_here1.anchor_string = '/sn1/'
     sign_here1.anchor_units = 'pixels'
     sign_here1.anchor_x_offset = '20'
@@ -59,16 +83,6 @@ class ESign::Eg021PhoneAuthenticationService
       signHereTabs: [sign_here1]
     })
     signer1.tabs = signer1_tabs
-
-    signer1.id_check_configuration_name = 'Phone Auth $'
-
-    phoneAuth = DocuSign_eSign::RecipientPhoneAuthentication.new
-    phoneAuth.record_voice_print = 'false'
-    phoneAuth.validate_recip_provided_number = 'false'
-    phoneAuth.recip_may_provide_number = 'true'
-
-    phoneAuth.sender_provided_numbers = [request.params[:phoneNumber]]
-    signer1.phone_authentication = phoneAuth
 
     # Add the recipients to the Envelope object
     recipients = DocuSign_eSign::Recipients.new(
@@ -82,5 +96,28 @@ class ESign::Eg021PhoneAuthenticationService
     # Step 4. Call the eSignature REST API
     results = envelope_api.create_envelope args[:account_id], envelope_definition
     # ***DS.snippet.0.end
+  end
+
+  def get_workflow(args)
+    #Retrieve the workflow id
+    begin
+      workflow_details = create_account_api(args)
+      workflow_response = workflow_details.get_account_identity_verification(args[:account_id])
+
+      # Check that idv authentication is enabled
+      # The workflow ID is a hard-coded value which is unique to this phone authentication workflow
+      if workflow_response.identity_verification
+          session[:workflow_id] = "c368e411-1592-4001-a3df-dca94ac539ae"
+          return session[:workflow_id]
+      else
+          return None
+      end
+
+    rescue DocuSign_Admin::ApiError => e
+      error = JSON.parse e.response_body
+      @error_code = e.code
+      @error_message = error['error_description']
+      render 'ds_common/error'
+    end
   end
 end
