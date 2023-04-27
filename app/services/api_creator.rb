3cbc167129202ac80c78e5697fb91144ff785a6d
Inbar Gazit <inbar.gazit@docusign.com>
2023-04-27 11:10:01 -0700

    Trying to see if we can have two snippets with the same code in Ruby for codeDepo (DEVDOCS-10346)

--- app/services/api_creator.rb
+++ app/services/api_creator.rb
@@ -33,11 +33,15 @@ module ApiCreator
   def create_envelope_api(args)
     # Obtain your OAuth token
     # Step 2 start
+    #ds-snippet-start eSign29Step2
+    #ds-snippet-start eSign28Step2
     configuration = DocuSign_eSign::Configuration.new
     configuration.host = args[:base_path]
     api_client = DocuSign_eSign::ApiClient.new configuration
 
     api_client.default_headers['Authorization'] = "Bearer #{args[:access_token]}"
+    #ds-snippet-end eSign28Step2
+    #ds-snippet-end eSign29Step2
     # Step 2 end
     DocuSign_eSign::EnvelopesApi.new api_client
   end
