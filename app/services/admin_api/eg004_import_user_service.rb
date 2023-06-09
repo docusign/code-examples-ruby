3251208cedd926279f309aaa96c937edd21da1ec
Karissa Jacobsen <karissa.jacobsen@docusign.com>
2023-06-09 10:40:32 -0700

    DEVDOCS-10502

--- app/services/admin_api/eg004_import_user_service.rb
+++ app/services/admin_api/eg004_import_user_service.rb
@@ -6,20 +6,20 @@ class AdminApi::Eg004ImportUserService
   end
 
   def worker
-    # Step 2 start
+    #ds-snippet-start:Admin4Step2
     configuration = DocuSign_Admin::Configuration.new
     configuration.host = Rails.configuration.admin_host
 
     api_client = DocuSign_Admin::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
-    # Step 2 end
+    #ds-snippet-end:Admin4Step2
 
-    # Step 3 start
+    #ds-snippet-start:Admin4Step3
     csv_file_data = File.open(args[:csv_file_path]).read
     csv_file_data = csv_file_data.gsub('{account_id}', args[:account_id])
 
     @bulk_imports_api = DocuSign_Admin::BulkImportsApi.new(api_client)
     @bulk_imports_api.create_bulk_import_add_users_request(args[:organization_id], csv_file_data)
-    # Step 3 end
+    #ds-snippet-end:Admin4Step3
   end
 end
