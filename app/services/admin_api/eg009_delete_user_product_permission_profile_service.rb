69697de7b3319ef1275b1cac71d284385c7ff9d2
Karissa Jacobsen <karissa.jacobsen@docusign.com>
2023-06-09 11:07:25 -0700

    DEVDOCS-10456

--- app/services/admin_api/eg009_delete_user_product_permission_profile_service.rb
+++ app/services/admin_api/eg009_delete_user_product_permission_profile_service.rb
@@ -8,22 +8,22 @@ class AdminApi::Eg009DeleteUserProductPermissionProfileService
   end
 
   def worker
-    # Step 2 start
+    #ds-snippet-start:Admin9Step2
     configuration = DocuSign_Admin::Configuration.new
     configuration.host = Rails.configuration.admin_host
 
     api_client = DocuSign_Admin::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
-    # Step 2 end
+    #ds-snippet-end:Admin9Step2
 
-    # Step 3 start
+    #ds-snippet-start:Admin9Step4
     user_product_profile_delete_request = DocuSign_Admin::UserProductProfileDeleteRequest.new({ 'user_email' => args[:email], 'product_ids' => [args[:product_id]] })
-    # Step 3 end
+    #ds-snippet-end:Admin9Step4
 
-    # Step 4 start
+    #ds-snippet-start:Admin9Step5
     product_permission_profiles_api = DocuSign_Admin::ProductPermissionProfilesApi.new(api_client)
     product_permission_profiles_api.remove_user_product_permission(args[:organization_id], args[:account_id], user_product_profile_delete_request)
-    # Step 4 end
+    #ds-snippet-end:Admin9Step5
   end
 
   def get_permission_profiles_by_email
@@ -33,6 +33,7 @@ class AdminApi::Eg009DeleteUserProductPermissionProfileService
     api_client = DocuSign_Admin::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
 
+    #ds-snippet-start:Admin9Step3
     product_permission_profiles_api = DocuSign_Admin::ProductPermissionProfilesApi.new(api_client)
 
     options = DocuSign_Admin::GetUserProductPermissionProfilesByEmailOptions.new
@@ -40,5 +41,6 @@ class AdminApi::Eg009DeleteUserProductPermissionProfileService
 
     product_permission_profiles = product_permission_profiles_api.get_user_product_permission_profiles_by_email(args[:organization_id], args[:account_id], options)
     product_permission_profiles.as_json['product_permission_profiles']
+    #ds-snippet-end:Admin9Step3
   end
 end
