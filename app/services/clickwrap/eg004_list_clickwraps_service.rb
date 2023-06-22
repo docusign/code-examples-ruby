2cf9db4398bf991f72b69c92abb32a8ab85f3ec0
Inbar Gazit <inbar.gazit@docusign.com>
2023-06-22 09:36:47 -0700

    DEVDOCS-10231 - adding codeDepot markers

--- app/services/clickwrap/eg004_list_clickwraps_service.rb
+++ app/services/clickwrap/eg004_list_clickwraps_service.rb
@@ -9,16 +9,20 @@ class Clickwrap::Eg004ListClickwrapsService
 
   def worker
     # Step 2. Construct your API headers
+    #ds-snippet-start:Click4Step2
     configuration = DocuSign_Click::Configuration.new
     configuration.host = args[:base_path]
 
     api_client = DocuSign_Click::ApiClient.new configuration
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
+    #ds-snippet-end:Click4Step2
 
     # Step 3. Call the Click API
+    #ds-snippet-start:Click4Step3
     accounts_api = DocuSign_Click::AccountsApi.new(api_client)
     accounts_api.get_clickwraps(
       args[:account_id]
     )
+    #ds-snippet-end:Click4Step3
   end
 end
