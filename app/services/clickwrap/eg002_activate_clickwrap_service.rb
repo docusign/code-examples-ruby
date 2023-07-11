a8934e2914375ac22dfcb842e5f6c9737bb7691e
Inbar Gazit <inbar.gazit@docusign.com>
2023-07-11 16:19:24 -0700

    DEVDOCS-10513 - adding codeDepot markers

--- app/services/clickwrap/eg002_activate_clickwrap_service.rb
+++ app/services/clickwrap/eg002_activate_clickwrap_service.rb
@@ -9,17 +9,22 @@ class Clickwrap::Eg002ActivateClickwrapService
 
   def worker
     # Step 2. Construct your API headers
+    #ds-snippet-start:Click2Step2
     configuration = DocuSign_Click::Configuration.new
     configuration.host = args[:base_path]
 
     api_client = DocuSign_Click::ApiClient.new configuration
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
+    #ds-snippet-end:Click2Step2
 
     # Step 3. Construct the request body
     # Create a clickwrap request model
+    #ds-snippet-start:Click2Step3
     clickwrap_request = DocuSign_Click::ClickwrapRequest.new(status: 'active')
+    #ds-snippet-end:Click2Step3
 
     # Step 4. Call the Click API
+    #ds-snippet-start:Click2Step4
     accounts_api = DocuSign_Click::AccountsApi.new(api_client)
     accounts_api.update_clickwrap_version(
       args[:account_id],
@@ -27,6 +32,7 @@ class Clickwrap::Eg002ActivateClickwrapService
       1,
       clickwrap_request
     )
+    #ds-snippet-end:Click2Step4
   end
 
   def get_inactive_clickwraps(statuses)
