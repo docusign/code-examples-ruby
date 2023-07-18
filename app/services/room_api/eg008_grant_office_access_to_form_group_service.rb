4162e57504ed2a8cbc0ba8609a68c8a69f1007fb
Inbar Gazit <inbar.gazit@docusign.com>
2023-07-18 10:20:07 -0700

    DEVDOCS-10627 - fixing

--- app/services/room_api/eg008_grant_office_access_to_form_group_service.rb
+++ app/services/room_api/eg008_grant_office_access_to_form_group_service.rb
@@ -8,22 +8,22 @@ class RoomApi::Eg008GrantOfficeAccessToFormGroupService
   end
 
   def worker
-    #ds-snippet-start:Rooms1Step2
+    #ds-snippet-start:Rooms8Step2
     configuration = DocuSign_Rooms::Configuration.new
     configuration.host = Rails.configuration.rooms_host
 
     api_client = DocuSign_Rooms::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
-    #ds-snippet-end:Rooms1Step2
+    #ds-snippet-end:Rooms8Step2
 
-    #ds-snippet-start:Rooms1Step5
+    #ds-snippet-start:Rooms8Step5
     form_groups_api = DocuSign_Rooms::FormGroupsApi.new(api_client)
     begin
       response = form_groups_api.grant_office_access_to_form_group(args[:form_group_id], args[:office_id], args[:account_id])
     rescue Exception
       return { exception: 'Failed to grant office access to a form group' }
     end
-    #ds-snippet-end:Rooms1Step5
+    #ds-snippet-end:Rooms8Step5
     response
   end
 end
