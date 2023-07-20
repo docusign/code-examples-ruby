5d92711062782170728b383465c93dbaf9b3add0
Inbar Gazit <inbar.gazit@docusign.com>
2023-07-20 12:14:50 -0700

    Updating comments

--- app/services/room_api/eg005_get_rooms_with_filters_service.rb
+++ app/services/room_api/eg005_get_rooms_with_filters_service.rb
@@ -11,14 +11,20 @@ class RoomApi::Eg005GetRoomsWithFiltersService
     configuration = DocuSign_Rooms::Configuration.new
     configuration.host = Rails.configuration.rooms_host
 
+    #ds-snippet-start:Rooms5Step2
     api_client = DocuSign_Rooms::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
+    #ds-snippet-end:Rooms5Step2
 
+    #ds-snippet-start:Rooms5Step3
     filters = DocuSign_Rooms::GetRoomsOptions.new
     filters.field_data_changed_start_date = args[:date_from]
     filters.field_data_changed_end_date = args[:date_to]
+    #ds-snippet-end:Rooms5Step3
 
+    #ds-snippet-start:Rooms5Step4
     rooms_api = DocuSign_Rooms::RoomsApi.new(api_client)
     rooms_api.get_rooms(args[:account_id], filters)
+    #ds-snippet-end:Rooms5Step4
   end
 end
