f8ddfebb7bc8b989679990450b2540304554e4c3
Paige Rossi <paige.rossi@docusign.com>
2023-07-27 14:34:48 -0700

    DEVDOCS-10521 codeDepot markers

--- app/services/monitor_api/eg001_get_monitoring_dataset_service.rb
+++ app/services/monitor_api/eg001_get_monitoring_dataset_service.rb
@@ -8,19 +8,19 @@ class MonitorApi::Eg001GetMonitoringDatasetService
   end
 
   def worker
-    # step 2 start
+    #ds-snippet-start:Monitor1Step2
     configuration = DocuSign_Monitor::Configuration.new
     configuration.host = Rails.configuration.monitor_host
     configuration.debugging = true
     api_client = DocuSign_Monitor::ApiClient.new configuration
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
-    # step 2 end
+    #ds-snippet-end:Monitor1Step2
 
-    # step 3 start
+    #ds-snippet-start:Monitor1Step3
     monitor_api = DocuSign_Monitor::DataSetApi.new(api_client)
     begin
       @response = monitor_api.get_stream(args[:data_set_name], args[:version]).data
-      # step 3 end
+    #ds-snippet-end:Monitor1Step3
     rescue StandardError
       # error, probalby no Monitor enabled
       @response = 'Monitor not enabled'
