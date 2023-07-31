8d58f9357d79250e8f25786f5a1c70cbd59cef36
Inbar Gazit <inbar.gazit@docusign.com>
2023-07-31 15:23:23 -0700

    DEVDOCS-10393 - adding codeDepot markers

--- app/services/room_api/eg004_add_forms_to_room_service.rb
+++ app/services/room_api/eg004_add_forms_to_room_service.rb
@@ -8,20 +8,26 @@ class RoomApi::Eg004AddFormsToRoomService
   end
 
   def worker
+    #ds-snippet-start:Rooms4Step2
     configuration = DocuSign_Rooms::Configuration.new
     configuration.host = Rails.configuration.rooms_host
 
     api_client = DocuSign_Rooms::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
+    #ds-snippet-end:Rooms4Step2
 
+    #ds-snippet-start:Rooms4Step4
     rooms_api = DocuSign_Rooms::RoomsApi.new(api_client)
 
     rooms_api.add_form_to_room(args[:room_id], args[:account_id], body(args[:form_id]))
+    #ds-snippet-end:Rooms4Step4
   end
 
   def body(form_id)
+    #ds-snippet-start:Rooms4Step3
     DocuSign_Rooms::FormForAdd.new({
                                      formId: form_id
                                    })
+    #ds-snippet-end:Rooms4Step3
   end
 end
