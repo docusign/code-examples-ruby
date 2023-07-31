fb2d5b9a8a29c063266027205c1bf97dec1fc7f6
Inbar Gazit <inbar.gazit@docusign.com>
2023-07-31 14:59:06 -0700

    DEVDOCS-10522 - adding codeDepot markers

--- app/services/room_api/eg003_export_data_from_room_service.rb
+++ app/services/room_api/eg003_export_data_from_room_service.rb
@@ -8,13 +8,17 @@ class RoomApi::Eg003ExportDataFromRoomService
   end
 
   def worker
+    #ds-snippet-start:Rooms3Step2
     configuration = DocuSign_Rooms::Configuration.new
     configuration.host = Rails.configuration.rooms_host
 
     api_client = DocuSign_Rooms::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
+    #ds-snippet-end:Rooms3Step2
 
+    #ds-snippet-start:Rooms3Step3
     rooms_api = DocuSign_Rooms::RoomsApi.new(api_client)
     rooms_api.get_room_field_data(args[:room_id], args[:account_id])
+    #ds-snippet-end:Rooms3Step3
   end
 end
