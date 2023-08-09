cae358b6604968a4d2f495950eb3afd2696ee085
Raileen Del Rosario <raileendr@gmail.com>
2023-08-09 14:28:06 -0700

    DEVDOCS-10363 - adding codeDepot markers

--- app/services/e_sign/eg028_brands_creating_service.rb
+++ app/services/e_sign/eg028_brands_creating_service.rb
@@ -19,10 +19,14 @@ class ESign::Eg028BrandsCreatingService
     api_client.default_headers['Authorization'] = "Bearer #{args[:access_token]}"
 
     # Step 3: Construct your request body
+    #ds-snippet-start:eSign28Step3
     accounts_api = DocuSign_eSign::AccountsApi.new api_client
     params = { brandName: args[:brandName], defaultBrandLanguage: args[:defaultBrandLanguage] }
+    #ds-snippet-end:eSign28Step3
 
     # Step 4: Call the eSignature API
+    #ds-snippet-start:eSign28Step4
     accounts_api.create_brand(args[:account_id], params)
+    #ds-snippet-end:eSign28Step4
   end
 end
