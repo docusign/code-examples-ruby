d5468c2a1ea6407e31d312cbaf7ca625101e384d
Raileen Del Rosario <raileendr@gmail.com>
2023-09-06 15:02:39 -0700

    DEVDOCS-10571 adding codeDepot markers

--- app/services/e_sign/eg005_envelope_recipients_service.rb
+++ app/services/e_sign/eg005_envelope_recipients_service.rb
@@ -10,7 +10,9 @@ class ESign::Eg005EnvelopeRecipientsService
   end
 
   def worker
+    #ds-snippet-start:eSign5Step2
     envelope_api = create_envelope_api(args)
     envelope_api.list_recipients args[:account_id], args[:envelope_id]
+    #ds-snippet-end:eSign5Step2
   end
 end
