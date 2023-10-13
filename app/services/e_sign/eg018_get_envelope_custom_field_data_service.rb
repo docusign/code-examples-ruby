b50b862ad46ca2fdf0c428466860001be97390af
Karissa Jacobsen <karissa.jacobsen@docusign.com>
2023-10-13 14:15:16 -0700

    DEVDOCS-10395 - codeDepot tags fix

--- app/services/e_sign/eg018_get_envelope_custom_field_data_service.rb
+++ app/services/e_sign/eg018_get_envelope_custom_field_data_service.rb
@@ -10,8 +10,8 @@ class ESign::Eg018GetEnvelopeCustomFieldDataService
   end
 
   def worker
-    #ds-snippet-start:eSign19Step3
+    #ds-snippet-start:eSign18Step3
     create_envelope_api(args).list_custom_fields args[:account_id], args[:envelope_id]
-    #ds-snippet-end:eSign19Step3
+    #ds-snippet-end:eSign18Step3
   end
 end
