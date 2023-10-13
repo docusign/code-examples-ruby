132d2c90ac1a79f8b9c7c1fa4ac7467f2b19ab3e
Karissa Jacobsen <karissa.jacobsen@docusign.com>
2023-10-13 14:12:36 -0700

    DEVDOCS-10395 - codeDepot tags

--- app/services/e_sign/eg018_get_envelope_custom_field_data_service.rb
+++ app/services/e_sign/eg018_get_envelope_custom_field_data_service.rb
@@ -10,7 +10,8 @@ class ESign::Eg018GetEnvelopeCustomFieldDataService
   end
 
   def worker
-    # Step 3. Call the eSignature REST API
+    #ds-snippet-start:eSign19Step3
     create_envelope_api(args).list_custom_fields args[:account_id], args[:envelope_id]
+    #ds-snippet-end:eSign19Step3
   end
 end
