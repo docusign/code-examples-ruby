d53f8d51e93c5a8d102f35c4391bed7d07a0316b
Raileen Del Rosario <raileendr@gmail.com>
2023-10-20 14:44:12 -0700

    DEVDOCS-10294 adding codeDepot markers

--- app/services/e_sign/eg003_list_envelopes_service.rb
+++ app/services/e_sign/eg003_list_envelopes_service.rb
@@ -10,17 +10,19 @@ class ESign::Eg003ListEnvelopesService
   end
 
   def worker
-    # Step 1. List the envelopes
+    # List the envelopes
     # The Envelopes::listStatusChanges method has many options
     # See https://developers.docusign.com/esign-rest-api/reference/Envelopes/Envelopes/listStatusChange#
     # The list status changes call requires at least a from_date OR
     # a set of envelopeIds. Here we filter using a from_date.
     # Here we set the from_date to filter envelopes for the last month
     # Use ISO 8601 date format
+    #ds-snippet-start:eSign3Step2
     envelope_api = create_envelope_api(args)
     options = DocuSign_eSign::ListStatusChangesOptions.new
     options.from_date = (Date.today - 30).strftime('%Y/%m/%d')
     # Exceptions will be caught by the calling function
     envelope_api.list_status_changes args[:account_id], options
+    #ds-snippet-end:eSign3Step2
   end
 end
