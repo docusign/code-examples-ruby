098fe9bcba660d186b616bf9548f21c5ad141ff3
Aaron Wilde <52012222+AaronWDS@users.noreply.github.com>
2023-11-08 06:26:26 -0800

    DEVDOCS-10425 cd tags
    Signed-off-by: Aaron Wilde <52012222+AaronWDS@users.noreply.github.com>

--- app/services/room_api/eg006_create_an_external_form_fill_session_service.rb
+++ app/services/room_api/eg006_create_an_external_form_fill_session_service.rb
@@ -8,19 +8,21 @@ class RoomApi::Eg006CreateAnExternalFormFillSessionService
   end
 
   def worker
+    #ds-snippet-start:Rooms6Step2
     configuration = DocuSign_Rooms::Configuration.new
     configuration.host = Rails.configuration.rooms_host
-
     api_client = DocuSign_Rooms::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
+    #ds-snippet-end:Rooms6Step2
 
+    #ds-snippet-start:Rooms6Step4
     rooms_api = DocuSign_Rooms::ExternalFormFillSessionsApi.new(api_client)
-
     rooms_api.create_external_form_fill_session(args[:account_id], body(args))
+    #ds-snippet-end:Rooms6Step4
   end
 
   private
-
+  #ds-snippet-start:Rooms6Step3
   def body(args)
     DocuSign_Rooms::ExternalFormFillSessionForCreate.new({
                                                            formId: args[:form_id],
@@ -28,4 +30,5 @@ class RoomApi::Eg006CreateAnExternalFormFillSessionService
                                                            xFrameAllowedUrl: "http://#{args[:allowed_host]}"
                                                          })
   end
+  #ds-snippet-end:Rooms6Step3
 end
