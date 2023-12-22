822c9f7b7e0a0c541ffb54affda3d7d138edc86b
RomanBachaloSigmaSoftware <roman.bachalo@sigma.software>
2023-12-22 14:04:51 +0200

    fix monitor example

--- app/services/monitor_api/eg001_get_monitoring_dataset_service.rb
+++ app/services/monitor_api/eg001_get_monitoring_dataset_service.rb
@@ -19,11 +19,28 @@ class MonitorApi::Eg001GetMonitoringDatasetService
     #ds-snippet-start:Monitor1Step3
     monitor_api = DocuSign_Monitor::DataSetApi.new(api_client)
     begin
-      @response = monitor_api.get_stream(args[:data_set_name], args[:version]).data
+      cursor_value = ''
+      limit = 100
+      function_results = []
+      options = DocuSign_Monitor::GetStreamOptions.new
+      options.limit = limit
+
+      loop do
+        options.cursor = cursor_value unless cursor_value.empty?
+
+        cursored_results = monitor_api.get_stream(args[:data_set_name], args[:version], options)
+        end_cursor = cursored_results.end_cursor
+
+        break if cursor_value == end_cursor
+
+        cursor_value = end_cursor
+        function_results.push(cursored_results.data)
+      end
+      @response = function_results
     #ds-snippet-end:Monitor1Step3
-    rescue StandardError
+    rescue Exception => e
       # error, probalby no Monitor enabled
-      @response = 'Monitor not enabled'
+      @response = e
     else
       Rails.logger.info 'Responses for loops are displayed here. Only the final loop is displayed on the response page'
       Rails.logger.info @response.inspect
