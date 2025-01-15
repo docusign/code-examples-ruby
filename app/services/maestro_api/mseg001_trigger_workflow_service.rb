4ef6b53399d5acfb636b75ca2ff3a5edbb558cef
Paige Rossi <paige.rossi@docusign.com>
2025-01-14 16:20:53 -0800

    removing Maestro codeDepot

--- app/services/maestro_api/mseg001_trigger_workflow_service.rb
+++ app/services/maestro_api/mseg001_trigger_workflow_service.rb
@@ -9,13 +9,11 @@ class MaestroApi::Mseg001TriggerWorkflowService
   end
 
   def get_workflow_definitions
-    #ds-snippet-start:Maestro1Step2
     configuration = DocuSign_Maestro::Configuration.new
     configuration.host = args[:base_path]
 
     api_client = DocuSign_Maestro::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
-    #ds-snippet-end:Maestro1Step2
 
     workflow_management_api = DocuSign_Maestro::WorkflowManagementApi.new(api_client)
 
@@ -31,10 +29,8 @@ class MaestroApi::Mseg001TriggerWorkflowService
     api_client = DocuSign_Maestro::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
 
-    #ds-snippet-start:Maestro1Step3
     workflow_management_api = DocuSign_Maestro::WorkflowManagementApi.new(api_client)
     workflow_management_api.get_workflow_definition(args[:account_id], args[:workflow_id])
-    #ds-snippet-end:Maestro1Step3
   end
 
   def trigger_workflow(workflow)
@@ -44,7 +40,6 @@ class MaestroApi::Mseg001TriggerWorkflowService
     api_client = DocuSign_Maestro::ApiClient.new(configuration)
     api_client.set_default_header('Authorization', "Bearer #{args[:access_token]}")
 
-    #ds-snippet-start:Maestro1Step4
     trigger_payload = DocuSign_Maestro::TriggerPayload.new
     trigger_payload.instance_name = args[:instance_name]
     trigger_payload.participants = {}
@@ -61,11 +56,8 @@ class MaestroApi::Mseg001TriggerWorkflowService
     trigger_options = DocuSign_Maestro::TriggerWorkflowOptions.new
     trigger_options.mtid = mtid
     trigger_options.mtsec = mtsec
-    #ds-snippet-end:Maestro1Step4
 
-    #ds-snippet-start:Maestro1Step5
     workflow_trigger_api = DocuSign_Maestro::WorkflowTriggerApi.new(api_client)
     workflow_trigger_api.trigger_workflow(args[:account_id], args[:workflow_id], trigger_payload, trigger_options)
-    #ds-snippet-end:Maestro1Step5
   end
 end
